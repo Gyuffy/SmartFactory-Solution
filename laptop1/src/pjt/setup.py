24b7d0d11591a625d695d8be81c4dc538e5032b8
dan360 <dan360@naver.com>
2025-05-22 13:29:13 +0900

    환경변수 연동

---
+++ laptop1/src/pjt/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from glob import glob
+
+package_name = "pjt"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/launch", glob("launch/*.launch.py")),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="ssafy",
+    maintainer_email="dan360@naver.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "dobot_homing_node = pjt.dobot_homing:main",
+            "socket_server_node = pjt.socket_server:main",
+        ],
+    },
+)
